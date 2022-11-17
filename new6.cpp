43123990906e6f0fde4f18dbd2e2a3030a36ebcb
deep-yad <117725197+deep-yad@users.noreply.github.com>
2022-11-17 23:02:39 +0530

    Update new6.cpp

--- new6.cpp
+++ new6.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+extern const float pi= 3.142;
 using namespace std;
 
 float pow(float n1,float n2){
@@ -8,24 +9,30 @@ float pow(float n1,float n2){
             }
             return d;
         }
-int fact(int n){
-    int i=1;
-    for(int k=1;k<=n;k++){
-        i=i*k;
-    }
-    return i;
-}
-float sin(float n){
+
+       float fact(float num){
+           double result =1;
+           double num2= num;
+           for(int i=0; i<num;i++){
+               result = result*num2;
+               num2--;
+           }
+           return result;
+       }
+
+       float sin(float n){
     float s=0;
-    for(int k=1;k<=10;k++){
-        s=s+pow(-1,k+1)*(pow(n,2*k-2)/fact(2*k-2));
+    for(int k=1;k<=100;k++){
+        s=s+pow(n,2*k-1)*pow(-1,k+1)/fact(2*k-1);
+        
+        
     }
     return s;
 }
 float cos(float n){
-    float s =1;
-    for (int k=2; k<=10; k++){
-        s = s + pow(-1, k+1)*(pow(n, 2*k-2)/fact(2*k-2));
+    float s =0;
+    for (int k=1; k<=100; k++){
+        s = s + pow(n,2*k)*(pow(-1,k)/fact(2*k));
     }
     return s;
 }
@@ -46,34 +53,63 @@ float tan(float n){
         
 int main(){
     float n1,n2;
-    cout<<"input 2 numbers "<<endl;
-    cin>>n1>>n2;
-    char op;
-    cout<<"input an operator "<<endl;
-    cin>>op;
-    switch(op)
+     int n;
+     label:
+     cout<<" press 1 for add/n press 2 for substraction/n press 3 for multiplication/n press 4 for division/n press 5 for power/n press 6,7,8 for trigonometry functions/n press 9 to exit "<<endl;
+     cin>>n;
+     switch (n)
+        
     {
-        case '+':
-        cout<<n1+n2<<endl;
-        break;
-        case '-':
-        cout<<n1-n2<<endl;
-        break;
-        case '*':
-        cout<<n1*n2<<endl;
-        break;
-        case '/':
-        cout<<n1/n2<<endl;
-        break;
-        case 'e':
-        cout<<pow(n1, n2)<<endl;
-        break;
-        case 's':
-        cout<<sin(n1)<<endl;
         
-        
-        
+        case 1 :
 
+        cout<<"<enter two numbers";
+        cin>>n1>>n2;
+        cout<<n1+n2<<endl;
+        goto label;
+        
+        case 2:
+       
+         cout<<"<enter two numbers"<<endl;
+         cin>>n1>>n2;
+        cout<<n1-n2<<endl;
+        goto label;
+
+        case 3:
+         cout<<"<enter two numbers"<<endl;
+         cin>>n1>>n2;
+        cout<<n1*n2<<endl;
+        goto label;
+
+        case 4:
+         cout<<"<enter two numbers"<<endl;
+         cin>>n1>>n2;
+        cout<<n1/n2<<endl;
+        goto label;
+
+        case 5:
+        cout<<"<enter base and power"<<endl;
+        cin>>n1>>n2;
+        cout<<pow(n1, n2)<<endl;
+        goto label;
+
+        case 6:
+        cout<<" enter the angle "<<endl;
+        cin>>n1;
+        cout<<sin(n1)<<endl;
+        goto label;
+        
+          case 7:
+        cout<<" enter the angle "<<endl;
+        cin>>n1;
+        cout<<cos(n1)<<endl;
+        goto label;
+
+          case 8:
+        cout<<" enter the angle "<<endl;
+        cin>>n1;
+        cout<<tan(n1)<<endl;
+        goto label;
 
     }
     return 0;
